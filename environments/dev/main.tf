876d3a7180a5c7da3db78a5e3d87fc71b97605c2
ice1203 <kikuchi3588@gmail.com>
2025-02-14 07:02:05 +0900

    natをなくした

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -24,7 +24,7 @@ module "main_vpc" {
   public_subnets  = ["172.18.128.0/24", "172.18.129.0/24", "172.18.130.0/24"]
   intra_subnets   = ["172.18.131.0/24", "172.18.132.0/24", "172.18.133.0/24"]
 
-  enable_nat_gateway                   = true
+  enable_nat_gateway                   = false
   single_nat_gateway                   = false
   enable_vpn_gateway                   = false
   enable_dns_hostnames                 = true
